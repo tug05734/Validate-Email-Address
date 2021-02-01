36cd0cbbdd848a80e19a4d90d8d575060af07e1b
Rajat Rajan <tug05734@temple.edu>
2021-02-01 17:11:01 -0500

    Added scanner to get user input

--- ValidateEmail/src/validateemail/ValidateEmail.java
+++ ValidateEmail/src/validateemail/ValidateEmail.java
@@ -5,17 +5,40 @@
  */
 package validateemail;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  *
  * @author Rajat
  */
 public class ValidateEmail {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static String[] emails = new String[10];
+    private static boolean stillRunning = true;
+    private static int i = 0;
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter e-mail addresses, max limit 10. Enter 'x' to quit. ");
+        while(stillRunning){
+            String userInput = in.nextLine();
+            if(userInput.equalsIgnoreCase("x")){
+                stillRunning = false;
+            }else{
+                if(i < 10){
+                    emails[i] = userInput;
+                    i++;
+                }else{
+                    System.out.println("Max limit reached");
+                    stillRunning = false;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(emails));
     }
+    
+    
     
 }
